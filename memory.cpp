09e6b18367c1cac470118a84aa716f67aa6276eb
Zhilue Wang <zhilue.harry.wang@outlook.com>
2022-08-01 03:32:18 -0400

    modified memory read

--- memory.cpp
+++ memory.cpp
@@ -4,15 +4,16 @@
 namespace memory {
     using namespace std;
 
-    void writeArr(int arr[], size_t &size, int &step) {
+    void readArr(int arr[], size_t &size, int &step) {
+        int j = 0;
         for (size_t i = 0; i < size; i += step) {
-            arr[i] = 1;
+            j = arr[i];
         }
     }
 
     chrono::nanoseconds measure(int arr[], size_t &size, int &step) {
         auto before = chrono::steady_clock::now();
-        writeArr(arr, size, step);
+        readArr(arr, size, step);
         auto after = chrono::steady_clock::now();
 
         chrono::nanoseconds diff = after - before;
@@ -20,26 +21,26 @@ namespace memory {
     }
 
     void cleanUpCache() {
-        auto *arr = new int[256 * 1024 * 1024];
-        for (int i = 0; i < 256 * 1024 * 1024; ++i) {
+        auto *arr = new int[4 * 1024 * 1024];
+        for (int i = 0; i < 4 * 1024 * 1024; ++i) {
             arr[i] = 1;
         }
     }
 
     void demo() {
-        int step = 0;
-        size_t size = 256 * 1024 * 1024;
+        int step = 1;
+        size_t size = 4 * 1024 * 1024;
 
-        for (int i = 0; i < 32; ++i) {
-            step += 2;
+        for (int i = 0; i < 8; ++i) {
+            step *= 2;
             int *arr = new int[size]{};
 
             auto before = chrono::steady_clock::now();
-            writeArr(arr, size, step);
+            readArr(arr, size, step);
             auto after = chrono::steady_clock::now();
 
             chrono::nanoseconds diff = after - before;
-            cout << "step size: " << step << " & " << step * 4 << "  Bytes,  " << diff.count() << " ns" << endl;
+            cout << "step size: " << step * 4 << "  Bytes,  " << diff.count() << " ns" << endl;
 
             delete[] arr;
 
